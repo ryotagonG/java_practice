1ceca99b949cd216960093758239cb8e5259a000
ryotagonG <ryotapb715@gmail.com>
2021-12-14 18:15:07 +0900

    条件分岐

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 //変数
 let title = "Hello World!";
 
-// title = "Hello World2!!";
+//title = "Hello World2!!";
 
-// 定数
+//定数
 const bigBody = "Hi...Hello World!"
-// bigBody = "Hello world2!!"
+//bigBody = "Hello world2!!"
 
+//配列
+const tarou = ["サッカー","野球","水泳","英会話"];
 
-console.log(bigBody);
+//ループ文
+// let index = 0;
+// while(index < tarou.length){
+//   // 繰り返したい命令
+//   console.log(tarou[index]);
+//   index++;
+// }
+
+//if /else
+if(tarou.length > 3){
+  console.log("いいですね！");
+} else {
+  console.log("だめです！")
+}
